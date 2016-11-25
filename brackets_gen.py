578da59be31dc12826e0b819bc1c577ad43ea5ac
phozzy <arthur.fayzullin@gamail.com>
2016-11-25 21:42:31 +0500

    removing shlak

--- brackets_gen.py
+++ brackets_gen.py
@@ -3,13 +3,9 @@
 from functools import reduce
 from itertools import repeat
 
-def get_level(level):
-    # finita
-    return filter(lambda it: it.count('(') == level, get_raw_list(level))
-
 def get_raw_list(level):
     # returns desired level brackets raw list
-    return reduce(spawn_brackets, enumerate(repeat(level, 2 * level - 1)), ['('])
+    return reduce(spawn_brackets, repeat(level, 2 * level - 1), ['('])
 
 def spawn_brackets(accout, itout):
     # spawns brackets branches
@@ -19,12 +15,12 @@ def spawn_brackets(accout, itout):
         closing = it.count(')')
         if closing < opening: 
             acc.append(it + ')')
-        if opening < itout[1]:
+        if opening < itout:
             acc.append(it + '(')
     return acc
 
-print(get_raw_list(12))
-# print(get_raw_list(2))
-# print(get_raw_list(3))
-# print(get_raw_list(4))
-# print(get_raw_list(5))
+print(get_raw_list(1))
+print(get_raw_list(2))
+print(get_raw_list(3))
+print(get_raw_list(4))
+print(get_raw_list(5))
