032ccb1600acc85239680830f8fd01993b95d9a3
phozzy <arthur.fayzullin@gmail.com>
2017-02-18 17:15:23 +0500

    otimizing str to int and back

--- brackets_light.py
+++ brackets_light.py
@@ -3,7 +3,8 @@
 from itertools import accumulate
 
 def brackets(level):
-    return filter(lambda it: it.count('1') == level and not(any(block(it))), fmtdrange(level))
+    # return filter(lambda it: it.count('1') == level and not(any(block(it))), fmtdrange(level))
+    return filter(lambda itera: (lambda it: it.count('1') == level and not(any(block(it))) (bin(itera))), rawrange(level))
 
 def fmtdrange(level):
     return map(lambda x: bin(x).lstrip('0b'), rawrange(level))
@@ -12,7 +13,8 @@ def rawrange(level):
     return range(int('0b' + level *'10', base = 2), int('0b' + level * '1' + level * '0', base = 2) + 0b10, 0b10)
 
 def accum_ones(string):
-    return accumulate(map(int, string[::-1]))
+    # return accumulate(map(int, string[::-1]))
+    return accumulate(map(int, string.lstrip('0b')[::-1]))
 
 def block(string):
     return map(lambda it: it[1] > it[0] - it[1] + 1 ,enumerate(accum_ones(string)))
@@ -22,4 +24,6 @@ def convert_list(lists):
     return ''.join(map(lambda it: int(it) and '(' or ')', lists))
 
 for it in brackets(15):
-    print(convert_list(it))
+    # print(convert_list(it))
+    print(it)
+# a = list(brackets(15))
