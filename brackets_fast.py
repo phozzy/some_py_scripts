e833bae10da13e786e6ec0f07d290bf68f85e706
Файзуллин Артур Фанизович <fayzullin.a@bashkortostan.ru>
2017-02-23 11:02:27 +0500

    rewriten in functianal way

--- brackets_fast.py
+++ brackets_fast.py
@@ -9,12 +9,13 @@ def get_raw_list(level):
 
 def spawn_brackets(accout, itout):
     # spawns brackets branches
-    acc = []
-    for it in accout:
-        open_brackets, clos_brackets = it.count('('), it.count(')')
-        (clos_brackets < open_brackets) and acc.append(it + ')')
-        (open_brackets < itout) and acc.append(it + '(')
-    return acc
+    # acc = []
+    # for it in accout:
+        # open_brackets, clos_brackets = it.count('('), it.count(')')
+        # (clos_brackets < open_brackets) and acc.append(it + ')')
+        # (open_brackets < itout) and acc.append(it + '(')
+    # return acc
+    return reduce(lambda acc, it: ((lambda open_brackets, clos_brackets: acc + ((clos_brackets < open_brackets) and [it + ')'] or []) + ((open_brackets < itout) and [it + '('] or [])) (it.count('('), it.count(')'))), accout, [])
 
 print(get_raw_list(1))
 print(get_raw_list(2))
