8b290b5b91d7f519493746b9bcb455d84f5eedda
Ph0zzy <afayzullin@plussell.org>
2018-03-31 10:03:03 +0500

    tried to make more readable

--- attlassianQ1.py
+++ attlassianQ1.py
@@ -2,12 +2,13 @@ import functools
 
 def __compareWords__(acc, it):
     return (
-        (lambda length:
-            (it, length) if length % 2 == 0 and length > acc[1] else acc)
-        (len(it)))
+        (lambda length: (it, length) if length % 2 == 0 and length > acc[1] else acc)
+        (len(it))
+    )
 
 def longestEvenWord(sentence):                    
     return functools.reduce(
         __compareWords__,
         sentence.split(),
-        ('00', 0))[0]
+        ('00', 0)
+    )[0]
