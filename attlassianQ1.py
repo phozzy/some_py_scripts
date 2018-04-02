23708b64bdec00b22d0601dbc1397a65fc7fea36
Ph0zzy <afayzullin@plussell.org>
2018-04-02 12:31:54 +0500

    Added some comments

--- attlassianQ1.py
+++ attlassianQ1.py
@@ -1,14 +1,20 @@
-import functools
+from functools import reduce
 
-def __compareWords__(acc, it):
+def __compareWords__(accumulator, iterator):
+    # returns new accumulator
     return (
-        (lambda length: (it, length) if length % 2 == 0 and length > acc[1] else acc)
-        (len(it))
+        # if length of a new word is even and greater than length of previous accumulator
+        (lambda length: (iterator, length) if length % 2 == 0 and length > accumulator[1] else accumulator)
+        (len(iterator))
     )
 
 def longestEvenWord(sentence):                    
-    return functools.reduce(
+    # let's go through a list of words from the sentence
+    # to find out
+    return reduce(
+        # result of comparing lengths of words
         __compareWords__,
         sentence.split(),
+        # let's initiate zero condition, '00' should be returned if none succeded
         ('00', 0)
-    )[0]
+    )[0] # result is zero indexed member of tuple
