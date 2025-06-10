84c8d1779ed9d4bfccc7fc069239b7df225b2e3d
Artur Faizullin <artur.faizullin@aimms.com>
2025-06-10 12:13:36 +0200

    Convert to class

--- brackets_generator.py
+++ brackets_generator.py
@@ -1,46 +1,54 @@
 #!/usr/bin/env python3
 
 from itertools import accumulate
-# uncomment this for random range, comment for straight range
-# from random import randrange
+from typing import Iterator
 
-def brackets(level):
-    # return filter(lambda it: it.count('1') == level and not(any(block(it))), fmtdrange(level))
-    # return filter(lambda itera: (lambda it: it.count('1') == level and not(any(block(it))) (bin(itera))), rawrange(level))
-    return filter(lambda it: bin(it).count('1') == level and vodoo(it, level), rawrange(level))
+class BracketGenerator:
+    def __init__(self, level: int):
+        self.level: int = level
 
-def fmtdrange(level):
-    return map(lambda x: bin(x).lstrip('0b'), rawrange(level))
+    def brackets(self) -> Iterator[str]:
+        return map(
+            lambda it: self._convert_list(bin(it)[2:].zfill(self.level * 2)),
+            filter(
+                lambda it: all([
+                        bin(it).count('1') == self.level,
+                        self._is_valid_bracket_sequence(it),
+                ]),
+                self._rawrange()
+            )
+        )
 
-def rawrange(level):
-    # comment this for random range, uncomment for straight range
-    return range(startvalue(level), stopvalue(level), 2)
-    # uncomment this for random range, comment for straight range
-    # return randrange(startvalue(level), stopvalue(level), 2)
+    def _fmtdrange(self) -> Iterator[str]:
+        return map(lambda x: bin(x).lstrip('0b'), self._rawrange())
 
-def startvalue(level):
-    # returns start value of sequence
-    return sum(map(lambda it: 2 ** (2 * it + 1), range(level)))
+    def _rawrange(self) -> range:
+        return range(self._startvalue(), self._stopvalue(), 2)
 
-def stopvalue(level):
-    # returns end of the range
-    return 2 ** (2 * level) - 2 ** level + 2
+    def _startvalue(self) -> int:
+        return sum(map(lambda it: 2 ** (2 * it + 1), range(self.level)))
 
-def accum_ones(string):
-    return accumulate(map(int, string[::-1]))
-    # return accumulate(map(int, string.lstrip('0b')[::-1]))
+    def _stopvalue(self) -> int:
+        return 2 ** (2 * self.level) - 2 ** self.level + 2
 
-def block(string):
-    return map(lambda it: it[1] > it[0] - it[1] + 1 ,enumerate(accum_ones(string)))
+    def _accum_ones(self, string: str) -> Iterator[int]:
+        return accumulate(map(int, string[::-1]))
 
-def vodoo(value, level):
-    return not(any(map(lambda it: (value % (2 ** (it * 2 + 1))) >= (2 ** (2 * it + 1) - 2 ** it), range(1, level))))
+    def _block(self, string: str) -> Iterator[bool]:
+        return map(lambda it: it[1] > it[0] - it[1] + 1, enumerate(self._accum_ones(string)))
 
-def convert_list(lists):
-    # convert 1 to ( and 0 to )
-    return ''.join(map(lambda it: int(it) and '(' or ')', lists))
+    def _is_valid_bracket_sequence(self, value: int) -> bool:
+        return not any(
+            map(
+                lambda it: (value % (2 ** (it * 2 + 1))) >= (2 ** (2 * it + 1) - 2 ** it),
+                range(1, self.level)
+            )
+        )
 
-for it in brackets(15):
-    # print(convert_list(bin(it).lstrip('0b')))
-    print(it)
-# a = list(brackets(15))
+    def _convert_list(self, lists: str) -> str:
+        return ''.join(map(lambda it: '(' if int(it) else ')', lists))
+
+if __name__ == "__main__":
+    generator = BracketGenerator(15)
+    for it in generator.brackets():
+        print(it)
