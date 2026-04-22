bfe262710ad395db7d1260e5bbe12d3b9f3850e2
phozzy <arthur.fayzullin@gmail.com>
2026-04-22 08:37:47 +0000

    feat(devshell): Activate treesitters with the devshell

--- flake.nix
+++ flake.nix
@@ -16,6 +16,8 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          pythonGrammar = pkgs.vimPlugins.nvim-treesitter.grammarToPlugin
+            pkgs.vimPlugins.nvim-treesitter.builtGrammars.python;
         in
         {
           default = pkgs.mkShell {
@@ -23,18 +25,23 @@
               python3
               uv
               cocogitto
+              pyright
+              ruff
             ];
 
-            env = lib.optionalAttrs pkgs.stdenv.isLinux {
-              # Python libraries often load native shared objects using dlopen(3).
-              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
-              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
-            };
+            # env = lib.optionalAttrs pkgs.stdenv.isLinux {
+            #   # Python libraries often load native shared objects using dlopen(3).
+            #   # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+            #   LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+            # };
 
             shellHook = ''
               unset PYTHONPATH
               uv sync
               . .venv/bin/activate
+              export NVIM_FORMATTERS='{"python": ["ruff_format"]}'
+              export NVIM_LINTERS='{"python": ["ruff"]}'
+              export NVIM_TREESITTER_PARSER_PATH="${pythonGrammar}"
             '';
           };
         }
